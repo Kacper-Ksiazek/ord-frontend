336c145a3b172c9e3c8142d51d5f94e2d1cc7865
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-11-15 18:58:04 +0100

    feat: add dynamic theme support to app logo component

--- src/lib/components/app-logo/app-logo.svelte
+++ src/lib/components/app-logo/app-logo.svelte
@@ -1,23 +1,25 @@
 <script lang="ts">
-import LogoDark from '$lib/assets/images/logo-dark.png';
-import LogoLight from '$lib/assets/images/logo-light.png';
-import { themeStore } from '$lib/stores/theme.svelte';
+import clsx from 'clsx';
+import Logo from '$lib/assets/images/logo.svg.svelte';
+import { type Theme, themeStore } from '$lib/stores/theme.svelte';
 
 interface Props {
 	size?: 'sm' | 'md' | 'lg';
+	class?: string;
 }
 
-let { size = 'md' }: Props = $props();
+let { size = 'md', class: className = '' }: Props = $props();
 
 const sizeClasses = {
 	sm: 'h-6 w-auto',
 	md: 'h-12 w-auto',
 	lg: 'h-32 w-32'
 };
+
+const colorClasses: Record<Theme, string> = {
+	light: 'text-gray-900',
+	dark: 'text-gray-50'
+};
 </script>
 
-<img
-	src={themeStore.isDark ? LogoDark : LogoLight}
-	alt="App Logo"
-	class="{sizeClasses[size]} object-contain"
-/>
+<Logo class={clsx(sizeClasses[size], className, colorClasses[themeStore.theme])} />
