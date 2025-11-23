1dd2cf31b93c59d46020e2f530c58bf9880f2d6d
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-11-23 14:22:09 +0100

    Export SSE utilities from api-client

--- src/lib/api-client/index.ts
+++ src/lib/api-client/index.ts
@@ -3,3 +3,11 @@
 // Re-export all auth-related exports
 export * from './auth';
 export { api } from './axios';
+
+// Re-export SSE utilities
+export { createSSEStream, type SSEStreamType } from './utils/see';
+export type {
+	SSEEvent,
+	SSEStreamOptions,
+	SSEStreamState
+} from './utils/see.types';
