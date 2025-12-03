ea5171f494ba86b50c6aa401c090b7c2d448c4da
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-12-03 03:33:12 +0100

    refactor: simplify request AI message SSE stream type

--- src/lib/api-client/ongoing-conversation/sse/request-ai-message.ts
+++ src/lib/api-client/ongoing-conversation/sse/request-ai-message.ts
@@ -1,12 +1,11 @@
 import type { Observable } from 'rxjs';
-import type { StreamSimpleItem } from '$lib/types/conversation/api';
 import type { CreateOngoingConversationMessageRequest } from '$lib/types/ongoing-conversation/api/requests';
 import { createSSEStream } from '../../utils/sse';
 
-export function requestAIMessage<T = StreamSimpleItem>(
+export function requestAIMessage(
 	body: CreateOngoingConversationMessageRequest
-): Observable<T> {
-	return createSSEStream<T>('/api/v1/conversations/ongoing/ai/request-message', {
+): Observable<string> {
+	return createSSEStream<string>('/api/v1/conversations/ongoing/ai/request-message', {
 		method: 'POST',
 		body
 	});
