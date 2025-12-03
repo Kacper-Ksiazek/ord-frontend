001ef129de6a1c03681bb942941b6db7d35b4aea
Kacper Ksiazek <kacper.b.ksiazek@gmail.com>
2025-12-03 01:20:19 +0100

    feat: add ongoing-conversation API types

---
+++ src/lib/types/ongoing-conversation/api/requests.ts
@@ -0,0 +1,10 @@
+import type { components } from '@ord-api/ord-api-types';
+
+export type CreateOngoingConversationMessageRequest =
+	components['schemas']['CreateAIConversationMessageRequest'];
+
+export type SaveUserConversationMessageRequest =
+	components['schemas']['SaveUserConversationMessageRequest'];
+
+export type GetFeedbackOnUserConversationMessageRequest =
+	components['schemas']['GetFeedbackOnUserConversationMessageRequest'];
---
+++ src/lib/types/ongoing-conversation/api/responses.ts
@@ -0,0 +1,6 @@
+import type { components } from '@ord-api/ord-api-types';
+
+export type ReviewedUserConversationMessage =
+	components['schemas']['ReviewedUserConversationMessage'];
+
+export type ConversationMessageDTO = components['schemas']['ConversationMessageDTO'];
